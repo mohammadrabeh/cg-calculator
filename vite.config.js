41f81b58ac1af4be0dc187241c762faae0bc1192
mohammadrabeh <1996media8@gmail.com>
2025-11-22 18:41:58 +0300

    Update Vite config for base path and plugins

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-
+  base: '/cg-calculator/'
 })
-export default defineConfig({
-  base: "/https:/cg-calculator/",
-});
